bbbb71a62908e46c5e25d8b2a1aa853860d8333f
Marco Antonio <101368238+marcoemygdio@users.noreply.github.com>
2022-03-11 00:26:09 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask
+app = Flask('app')
+
+@app.route('/')
+def hello_world():
+  return '<h1>Olá Mundo!</h1>'
+
+@app.route('/unifran')
+def unifran():
+  return '<h2>Universidade de Franca</h2>'
+
+app.run(host='0.0.0.0', port=8080)
